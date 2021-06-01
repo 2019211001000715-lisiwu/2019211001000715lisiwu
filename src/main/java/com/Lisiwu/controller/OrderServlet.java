643595e09a2b982116616d7aee2bb2691ed01bca
20192110001000715-lisiwu <2096206640@qq.com>
2021-06-01 09:39:10 +0800

    HW week 14 commit Lab-1-Startup Code.zip

---
+++ src/main/java/com/Lisiwu/controller/OrderServlet.java
@@ -0,0 +1,38 @@
+package com.Lisiwu.controller;
+
+import com.Lisiwu.model.Payment;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.List;
+
+@WebServlet(name = "OrderServlet", value = "/order")
+public class OrderServlet extends HttpServlet {
+    Connection con=null;
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        con=(Connection) getServletContext().getAttribute("con");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        List<Payment> paymentTypeList=Payment.findAllPayment(con);
+        request.setAttribute("paymentTypeList",paymentTypeList);
+        String path="/WEB-INF/views/order.jsp";
+        request.getRequestDispatcher(path).forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
